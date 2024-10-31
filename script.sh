691dc373292f3fdad089b3c0c369c5e15cab8f0b
Mateus Pavei Menegon <ocanema@gmail.com>
2024-10-31 13:47:20 +0100

    commit

--- script.sh
+++ script.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        --username) username="$2"; shift ;;
-        --server) server="$2"; shift ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
-
 echo "Deploying to server $server with username $username"
